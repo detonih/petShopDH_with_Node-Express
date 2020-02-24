fd403627f90277a0d85105df9918e410e2848885
Henrique Detoni <detonihenrique@gmail.com>
2020-02-24 13:57:21 -0300

    cria estrutura do projeto e adiciona dependencias

---
+++ src/controllers/petsController.js
@@ -0,0 +1,1 @@
+const petsDatabase = require('../database/pets');
---
+++ src/database/pets.js
@@ -0,0 +1,49 @@
+const pets = [
+  {
+    nome: "Batman",
+    tipo: "cão",
+    raca: "labrador",
+    idade: 5,
+    genero: "M",
+    vacinado: false,
+    servicos: ["banho", "tosa"]
+  },
+  {
+    nome: "Costelinha",
+    tipo: "cão",
+    raca: "vira-lata",
+    idade: 10,
+    genero: "M",
+    vacinado: true,
+    servicos: ["banho"]
+  },
+  {
+    nome: "Scooby Doo",
+    tipo: "cão",
+    raca: "Dogue Alemão",
+    idade: 51,
+    genero: "M",
+    vacinado: false,
+    servicos: ["banho", "tosa"]
+  },
+  {
+    nome: "Tom",
+    tipo: "gato",
+    raca: "poodle",
+    idade: 5,
+    genero: "M",
+    vacinado: false,
+    servicos: ["corte de unha"]
+  },
+  {
+    nome: "Ada",
+    tipo: "iguana",
+    raca: "albina",
+    idade: 5,
+    genero: "F",
+    vacinado: true,
+    servicos: ["banho"]
+  }
+];
+
+module.exports = pets;
